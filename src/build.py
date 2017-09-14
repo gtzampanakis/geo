18199f96324a5abb64fdfd59ea87de83917c6aa9
Giorgos Tzampanakis <giorgos.tzampanakis@gmail.com>
2017-09-14 22:37:00 +0300

    Build process outputs a zip file

--- src/build.py
+++ src/build.py
@@ -5,24 +5,39 @@ import zipfile
 BASE_DIR = os.path.dirname(__file__)
 BUILD_DIR = os.path.join(BASE_DIR, 'build')
 LIB_DIR = os.path.join(BUILD_DIR, 'lib')
+ZIP_PATH = os.path.join(BASE_DIR, 'geotools.zip')
 
 if os.path.exists(BUILD_DIR):
     shutil.rmtree(BUILD_DIR)
 
+if os.path.exists(ZIP_PATH):
+    os.remove(ZIP_PATH)
+
 os.mkdir(BUILD_DIR)
 os.mkdir(LIB_DIR)
 
-for filename in [
+LIB_FILENAMES = [
     'datamerge.py',
     'error.py',
     'favicon.ico',
-    'fileio.py.',
+    'fileio.py',
     'geomath.py',
     'gui.py',
     'geotypes.py',
-]:
+]
+
+for filename in LIB_FILENAMES:
     shutil.copy(os.path.join(BASE_DIR, filename), LIB_DIR)
 
 shutil.copyfile(
     os.path.join(BASE_DIR, 'geotools.py'),
     os.path.join(BUILD_DIR, 'geotools.pyw'))
+
+with zipfile.ZipFile(ZIP_PATH, 'w') as z:
+    for filename in LIB_FILENAMES:
+        z.write(os.path.join(LIB_DIR, filename), os.path.join('lib', filename))
+    z.write(
+        os.path.join(BUILD_DIR, 'geotools.pyw'),
+        os.path.join('geotools.pyw'))
+    
+
